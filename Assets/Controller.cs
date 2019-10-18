51a726d0a53285230aca2c9943f0c0ef216555f8
troyfitzg <c16432792@mydit.ie>
2019-10-18 23:26:57 +0100

    Add files via upload

---
+++ Assets/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody player;
+
+    public float Speed = 100f;
+    public float force = 100f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        player.transform.Translate(0, 0, force * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * Speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+
+            player.transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * Speed, 0, 0);
+        }
+
+   
+    }
+}
---
+++ Assets/PlatformSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _platforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < _platforms.Length; i++)
+        {
+            Instantiate(_platforms[i], new Vector3(0, 0, i * 50), Quaternion.Euler(0, 0, 0));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
